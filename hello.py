b1106f01d85931bf41641c9d7b33184adb5dafa6
S2184285 <129802186+S2184285@users.noreply.github.com>
2025-11-24 23:22:30 -0500

    Added new message

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-#Nik Smith
-#11/24/2025
-#A simple program to print a message
+# Nik Smith
+# 11/24/2025
+# A simple program to print a message
 
-pring("Hello World!")
+# Nik Smith
+# 11/24/2025
+# A simple program to print a message
+
+print("Hello World!")
+
+print("Welcome to GitHub!")
